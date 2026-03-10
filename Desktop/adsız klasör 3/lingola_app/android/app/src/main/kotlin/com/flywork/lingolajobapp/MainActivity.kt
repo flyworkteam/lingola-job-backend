2235871f3bcd8461e4108b1f4b159d5626a54877
Kadir Karataş <kadirkaratass112@gmail.com>
2026-03-10 17:20:08 +0300

    Delete Desktop/adsız klasör 3/lingola_app directory

--- Desktop/adsız klasör 3/lingola_app/android/app/src/main/kotlin/com/flywork/lingolajobapp/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.flywork.lingolajobapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Desktop/adsız klasör 3/lingola_app/lib/Riverpod/Providers/word_repository_provider.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import 'package:lingola_app/Repositories/word_repository.dart';
-
-final wordRepositoryProvider = Provider<WordRepository>((ref) => WordRepository());
